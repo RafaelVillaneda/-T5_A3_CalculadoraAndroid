45085f131e415b7c687e506c56ebb0072a5df5c8
Rafael Eulalio Villaneda de la Torre <rafaeua2401@outlook.com>
2021-05-13 13:06:22 -0500

    Se hicieron los eventos de los botones excepto suma u mas menos

--- app/src/main/java/com/example/calculadoraandroid/MainActivity.java
+++ app/src/main/java/com/example/calculadoraandroid/MainActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     String operacionRelaizar="?";
     String operacion="";
-    String segundaParte="";
+    byte limiOp=1;
     TextView mostrar;
 
     @Override
@@ -58,4 +59,106 @@ public class MainActivity extends AppCompatActivity {
         operacion=operacion+"9";
         mostrar.setText(operacion);
     }
+    public void botonResiduo(View v){
+        if(limiOp>0) {
+            operacion = operacion + "%";
+            mostrar.setText(operacion);
+        }
+    }
+    public void botonCuadrado(View v){
+        double raiz=0.0;
+        if(!operacion.equals("")) {
+             raiz = Double.parseDouble(operacion);
+            raiz = Math.pow(raiz,2);
+        }
+        mostrar.setText(String.valueOf(raiz));
+    }
+    public void botonRaiz(View v) {
+        double raiz=0.0;
+        if(!operacion.equals("")) {
+             raiz = Double.parseDouble(operacion);
+            raiz = Math.sqrt(raiz);
+        }
+        mostrar.setText(String.valueOf(raiz));
+    }
+    public void botonCE(View v){
+        mostrar.setText("");
+    }
+    public void botonC(View v){
+        mostrar.setText("");
+    }
+    public void botonBorrar(View v){//"±"
+        String temp=operacion;
+        temp=temp.substring(0,temp.length()-1);
+        operacion=temp;
+        mostrar.setText(operacion);
+    }
+    public void botonpor(View v){
+        if(limiOp>0) {
+            limiOp--;
+            operacionRelaizar = "~";
+            operacion = operacion + "~";
+            mostrar.setText(operacion);
+        }
+    }
+    public void botonDiv(View v){
+        if(limiOp>0) {
+            limiOp--;
+            operacionRelaizar = "/";
+            operacion = operacion + "/";
+            mostrar.setText(operacion);
+        }
+    }
+    public void botonResta(View v){
+        if(limiOp>0) {
+            limiOp--;
+            operacionRelaizar = "-";
+            operacion = operacion + "-";
+            mostrar.setText(operacion);
+        }
+    }
+    public void botonSuma(View v){
+        if(limiOp>0) {
+            limiOp--;
+            operacionRelaizar = "±";
+            operacion = operacion + "±";
+            mostrar.setText(operacion);
+        }
+    }
+    public void igual(View v){
+        String[] parts;
+        limiOp++;
+        if(operacionRelaizar.equals("±")){
+            parts = operacionRelaizar.split("±");
+            double p1 = Double.parseDouble(parts[0]);
+            double p2 = Double.parseDouble(parts[1]);
+            p1 = p1 + p2;
+            mostrar.setText(String.valueOf(p1));
+            operacion="";
+        }else  if(operacionRelaizar.equals("-")) {
+            parts = operacion.split("-");
+            double p1 = Double.parseDouble(parts[0]);
+            double p2 = Double.parseDouble(parts[1]);
+            p1 = p1 - p2;
+            mostrar.setText(String.valueOf(p1));
+            operacion="";
+        }else  if(operacionRelaizar.equals("~")){
+            parts = operacion.split("~");//-----------------------------------
+            double p1=Double.parseDouble(parts[0]);
+            double p2=Double.parseDouble(parts[1]);
+            p1=p1*p2;
+            mostrar.setText(String.valueOf(p1));
+            operacion="";
+        }else  if(operacionRelaizar.equals("/")){
+            parts = operacion.split("/");
+            double p1=Double.parseDouble(parts[0]);
+            double p2=Double.parseDouble(parts[1]);
+            p1=p1/p2;
+            mostrar.setText(String.valueOf(p1));
+            operacion="";
+        }
+    }
+
+
+
 }
